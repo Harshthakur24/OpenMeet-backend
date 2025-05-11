6706773e69611ade6605c6d4c2157c1de56901fe
Harshthakur24 <thakur2004harsh@gmail.com>
2025-05-11 16:14:52 +0530

    Prepare for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,33 @@
-import { Socket } from "socket.io";
-import http from "http";
-
 import express from 'express';
-import { Server } from 'socket.io';
-import { UserManager } from "./managers/UserManger";
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import { UserManager } from './managers/UserManger';
 
 const app = express();
-const server = http.createServer(http);
-
-const io = new Server(server, {
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
   cors: {
-    origin: "*"
+    origin: '*',
+    methods: ['GET', 'POST']
   }
 });
 
 const userManager = new UserManager();
 
-io.on('connection', (socket: Socket) => {
-  console.log('a user connected');
-  userManager.addUser("randomName", socket);
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-    userManager.removeUser(socket.id);
-  })
+app.get('/', (req, res) => {
+  res.send('OpenMeet Backend Server is running!');
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+  userManager.addUser('randomName', socket);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    userManager.removeUser(socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
